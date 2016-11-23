cfbd61cb6634e63fcddf542ed6ed13577be55dec
Daniel Freire <danjfreire@gmail.com>
2016-11-23 10:02:14 -0200

    TelaFuncionarioAtualizada

--- src/br/ufrpe/sapientia/GUI/TelaInicialFunc.java
+++ src/br/ufrpe/sapientia/GUI/TelaInicialFunc.java
@@ -22,6 +22,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.FlowLayout;
+import br.ufrpe.sapientia.negocio.beans.*;
 
 public class TelaInicialFunc extends JFrame {
 
@@ -30,38 +31,40 @@ public class TelaInicialFunc extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private Usuario funcionario;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-				    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-				        if ("Nimbus".equals(info.getName())) {
-				            UIManager.setLookAndFeel(info.getClassName());
-				            break;
-				        }
-				    }
-				} catch (Exception e) {
-				    // If Nimbus is not available, you can set the GUI to another look and feel.
-				}
-				try {
-					TelaInicialFunc frame = new TelaInicialFunc();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	//public static void main(String[] args) {
+	//	EventQueue.invokeLater(new Runnable() {
+	//		public void run() {
+	//			try {
+	//			    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+	//			        if ("Nimbus".equals(info.getName())) {
+	//			            UIManager.setLookAndFeel(info.getClassName());
+	//			            break;
+	//			        }
+	//			    }
+	//			} catch (Exception e) {
+	//			    // If Nimbus is not available, you can set the GUI to another look and feel.
+	//			}
+	//			try {
+	//				TelaInicialFunc frame = new TelaInicialFunc();
+	//				frame.setVisible(true);
+	//			} catch (Exception e) {
+	//				e.printStackTrace();
+	//			}
+	//		}
+	//	});
+	//}
 
 	/**
 	 * Create the frame.
 	 */
-	public TelaInicialFunc() {
+	public TelaInicialFunc(Usuario u) {
 		
+		this.funcionario = u;
 		setTitle("Sapientia");
 		//setExtendedState( MAXIMIZED_BOTH );
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -332,7 +335,7 @@ public class TelaInicialFunc extends JFrame {
 		mntmHistricoDeEmprstimo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				TelaHistóricoLivros tela = new TelaHistóricoLivros();
+				TelaHistÃ³ricoLivros tela = new TelaHistÃ³ricoLivros();
 				desktopPane.add(tela);
 				try {
 					tela.setMaximum(true);
@@ -371,7 +374,7 @@ public class TelaInicialFunc extends JFrame {
 		mntmEmprestarLivro.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				TelaEmpréstimo tela = new TelaEmpréstimo();
+				TelaEmprÃ©stimo tela = new TelaEmprÃ©stimo();
 				desktopPane.add(tela);
 				try {
 					tela.setMaximum(true);
